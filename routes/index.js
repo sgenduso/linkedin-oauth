9b6ba6b6431956bb7413fb235ea8e8e34eb1ae64
Sam Genduso <sam.genduso@gmail.com>
2015-07-09 14:17:23 -0600

    API call to pull profile picture

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var unirest = require('unirest');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'LinkedIn Authorization' });
+  if(req.isAuthenticated()) {
+    unirest.get('https://api.linkedin.com/v1/people/~:(id,num-connections,picture-url)')
+      .header('Authorization', 'Bearer ' + req.user.token)
+      .header('x-li-format', 'json')
+      .end(function (response) {
+        res.render('index', { profile: response.body });
+      });
+  } else {
+    res.render('index', {  });
+  }
 });
 
 module.exports = router;
